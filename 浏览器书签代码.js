cf834712b51e1f70938144e4aed666b346518a0e
ezshine <ezshine@live.cn>
2021-04-21 10:24:20 +0800

    add color mode

--- 浏览器书签代码.js
+++ 浏览器书签代码.js
@@ -1,13 +1,22 @@
+//复制下面的代码，打开网址：https://www.sojson.com/yasuojs.html
+//粘贴后选择普通压缩，然后再粘贴到收藏夹网页地址栏中
+
 javascript:
-!(function(){
-	console.log("badapple effect enabled");
+(function(str,bgap=12,color=0){
+
+	var asciiList = str.split("");
 
 	function renderVideoFrame(videoDom) {
-		var asciiList = ['猿','帅','老','大', '　'];
 
-		var scale = parseInt(videoDom.videoHeight/parseFloat($(videoDom).css("height")));
-		var gap = 12/scale;
-		console.log(scale);
+		var scale =1;
+
+		if(videoDom.videoHeight>videoDom.videoWidth){
+			scale = parseInt(videoDom.videoHeight/parseFloat($(videoDom).css("height")));
+		}else{
+			scale = parseInt(videoDom.videoWidth/parseFloat($(videoDom).css("width")));
+		}
+
+		var gap = bgap/scale;
 
 		var videoSize = {width:parseFloat(videoDom.videoWidth/scale),height:parseFloat(videoDom.videoHeight/scale)};
 
@@ -42,43 +51,33 @@ javascript:
 				var position = (videoSize.width * h + w) * 4;
 				var r = imgData[position], g = imgData[position + 1], b = imgData[position + 2];
 
-				var gray = (r*30 + g*59 + b*11 + 50) / 100;
+				if(color){
+					ctx.fillStyle = "rgb("+r+","+g+","+b+")";
+					ctx.fillText(asciiList[0], w, h);
+				}else{
+					var gray = (r*30 + g*59 + b*11 + 50) / 100;
 
-				var i = Math.min(asciiList.length-1,parseInt(gray / (255 / asciiList.length)));
+					var i = Math.min(asciiList.length-1,parseInt(gray / (255 / asciiList.length)));
 
-				ctx.fillText(asciiList[i], w, h);
+					ctx.fillText(asciiList[i], w, h);
+				}
 			}
 		}
 	}
 
 	var videoDom = document.querySelector("video");
 	videoDom.style.display = "none";
-	videoDom.addEventListener('canplay',function(){
-		renderVideoFrame(videoDom);
-	});
 
 	videoDom.addEventListener('play',function(){
-		console.log("开始播放");
-		// btnPlayAndPause.innerText = "";
-
 		startRender();
 	});
 
 	//监听播放结束
-	videoDom.addEventListener('pause',function(){
-	    console.log("播放暂停");
-	    // btnPlayAndPause.innerText = "play";
-
+	function pauseAndStopHandler(){
 	    stopRender();
-	}); 
-
-	//监听播放结束
-	videoDom.addEventListener('ended',function(){
-	    console.log("播放结束");
-	    // btnPlayAndPause.innerText = "play";
-
-	    stopRender();
-	});
+	}
+	videoDom.addEventListener('pause',pauseAndStopHandler); 
+	videoDom.addEventListener('ended',pauseAndStopHandler);
 
 	var timerId;
 	function startRender() {
@@ -91,4 +90,6 @@ javascript:
 	function stopRender(){
 		cancelAnimationFrame(timerId);
 	}
-})()
+
+	console.log("badapple effect enabled");
+})("猿老帅大 ");
